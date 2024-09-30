8bd9089f68ce836b54cad51f34fb8d6053fee564
Priyanka Saini <priyankaeagle69@gmail.com>
2024-09-30 16:26:51 +0530

    again pushing new.py with ssh

--- new.py
+++ new.py
@@ -1,3 +1,7 @@
 print("hello")
+<<<<<<< HEAD
 ssh -i C:\Users\HP\Downloads\devm.pem -L 5901:localhost:5901 -L 6080:localhost:6080 -L 8888:localhost:8888 vmadmin@52.172.164.159
 git
+=======
+ssh -i C:\Users\HP\Downloads\devm.pem -L 5901:localhost:5901 -L 6080:localhost:6080 -L 8888:localhost:8888 vmadmin@52.172.164.159
+>>>>>>> parent of 93c7470 (deleted both the files)
